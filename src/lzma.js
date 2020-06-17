4b9161aaccf89ced20151a532f602ef6dc76cb76
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2020-06-17 15:49:09 +1000

    feat(lib): add promise-based compressP decompressP functions; simple error handling to pass errors from worker to caller

--- src/lzma.js
+++ src/lzma.js
@@ -33,6 +33,17 @@
         ///NOTE: Node.js needs something like "./" or "../" at the beginning.
         var lzma_worker = new Worker(lzma_path || "./lzma_worker-min.js");
         
+        /*
+        2020-06-17
+        Quick and dirty fix for error handling. The lzma worker just throws errors without passing
+        back the `data.callback_num` that it belongs to so we don't actually know which request failed.
+        
+        However, we effectively create a new worker for every encode/decode request so we can treat
+        this as being a one-shot thing, keep track of the last callback id that was generated and
+        assume that the error belongs to that id.
+        */
+        var last_callback_num = 0;
+        
         lzma_worker.onmessage = function (e) {
             if (e.data.action === action_progress) {
                 if (callback_obj[e.data.callback_num] && typeof callback_obj[e.data.callback_num].on_progress === "function") {
@@ -50,21 +61,31 @@
         
         /// Very simple error handling.
         lzma_worker.onerror = function(event) {
-            throw new Error(event.message + " (" + event.filename + ":" + event.lineno + ")");
+            if (callback_obj[last_callback_num] && typeof callback_obj[last_callback_num].on_error === "function") {
+                var error = new Error(event.message + " (" + event.filename + ":" + event.lineno + ")");
+                callback_obj[last_callback_num].on_error(error);
+                
+                /// Since the (de)compression is complete, the callbacks are no longer needed.
+                delete callback_obj[last_callback_num];
+            }
         };
         
         return (function () {
             
-            function send_to_worker(action, data, mode, on_finish, on_progress) {
+            function send_to_worker(action, data, mode, on_finish, on_progress, on_error) {
                 var callback_num;
                 
                 do {
                     callback_num = Math.floor(Math.random() * (10000000));
                 } while(typeof callback_obj[callback_num] !== "undefined");
                 
+                // keep track of callback id
+                last_callback_num = callback_num;
+                
                 callback_obj[callback_num] = {
                     on_finish:   on_finish,
-                    on_progress: on_progress
+                    on_progress: on_progress,
+                    on_error: on_error
                 };
                 
                 lzma_worker.postMessage({
@@ -76,11 +97,46 @@
             }
             
             return {
-                compress: function compress(string, mode, on_finish, on_progress) {
-                    send_to_worker(action_compress, String(string), mode, on_finish, on_progress);
+                /**
+                 * Promise-based versions of compress/decompress.
+                 * 
+                 * @since v2.1.x
+                 */
+                compressP: function compressP(string, mode){
+                    return new Promise(function(resolve, reject){
+                        function on_finish(result){
+                            resolve(result);
+                        }
+                        var on_progress = null;
+                        function on_error(error){
+                            reject(error);
+                        }
+                        send_to_worker(action_compress, String(string), mode, on_finish, on_progress, on_error);
+                    });
                 },
-                decompress: function decompress(byte_arr, on_finish, on_progress) {
-                    send_to_worker(action_decompress, byte_arr, false, on_finish, on_progress);
+                decompressP: function decompressP(byte_arr){
+                    return new Promise(function(resolve, reject){
+                        function on_finish(result/*: string | false*/){
+                            resolve(result);
+                        }
+                        var on_progress = null;
+                        function on_error(error){
+                            reject(error);
+                        }
+                        send_to_worker(action_decompress, byte_arr, false, on_finish, on_progress, on_error);
+                    });
+                },
+    
+                /**
+                 * Callback-based functions
+                 * 
+                 * `on_error` available since v2.1.x
+                 */
+                compress: function compress(string, mode, on_finish, on_progress, on_error) {
+                    send_to_worker(action_compress, String(string), mode, on_finish, on_progress, on_error);
+                },
+                decompress: function decompress(byte_arr, on_finish, on_progress, on_error) {
+                    send_to_worker(action_decompress, byte_arr, false, on_finish, on_progress, on_error);
                 }
             };
         }());
